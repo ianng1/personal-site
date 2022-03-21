d74007c1e1084f207a54c7c5601ada5609a7dc75
Peng Wah Ng <pengwah@Pengs-iMac.attlocal.net>
2022-03-20 22:59:51 -0700

    Changing about page

---
+++ src/pages/about.js
@@ -0,0 +1,51 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+const ContactPage = ({
+  data: {
+    site
+  },
+}) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Contact — {site.siteMetadata.title}</title>
+        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+      </Helmet>
+      <div className="blog-post-container">
+        <article className="post">
+          
+          {!frontmatter.thumbnail && (
+            <div className="post-thumbnail">
+              <h1 className="post-title">About Me</h1>
+              <div className="post-meta">Yee</div>
+            </div>
+          )}
+          {!!frontmatter.thumbnail && (
+            <div className="post-thumbnail" style={{backgroundImage: `url(${frontmatter.thumbnail})`}}>
+              <h1 className="post-title">POST title 2</h1>
+              <div className="post-meta">POST date 2</div>
+            </div>
+          )}
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </article>
+      </div>
+    </Layout>
+  )
+}
+export default ContactPage
+export const pageQuery = graphql`
+  query ContactPageQuery{
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
